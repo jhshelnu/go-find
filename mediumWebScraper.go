1873fbade3388a621e714d26d9607409f960f20f
harrison <harrisonshelnutt@gmail.com>
2024-07-05 11:24:37 -0600

    slightly improved web scraper performance

--- mediumWebScraper.go
+++ mediumWebScraper.go
@@ -11,26 +11,27 @@ var mediumArticleTextSelector = "article p.pw-post-body-paragraph"
 
 type MediumWebScraper struct {
 	delegate *colly.Collector
-	buffer   *string // what the delegate web scraper (colly) will write to (note: not thread safe!)
+	buffer   *strings.Builder // what the delegate web scraper (colly) will write to (note: not thread safe!)
 }
 
 func MakeMediumWebScraper() MediumWebScraper {
 	delegate := colly.NewCollector(colly.URLFilters(mediumRegexp))
-	buffer := new(string)
+	buffer := new(strings.Builder)
 	mediumWebScraper := MediumWebScraper{delegate: delegate, buffer: buffer}
 	delegate.OnHTML(mediumArticleTextSelector, func(e *colly.HTMLElement) {
-		*mediumWebScraper.buffer += " " + e.Text
+		mediumWebScraper.buffer.WriteString(" ")
+		mediumWebScraper.buffer.WriteString(e.Text)
 	})
 	return mediumWebScraper
 }
 
 func (scraper MediumWebScraper) getArticleText(url string) (string, error) {
-	*scraper.buffer = ""
+	scraper.buffer.Reset()
 
 	err := scraper.delegate.Visit(url)
 	if err != nil {
 		return "", err
 	}
 
-	return strings.Clone(*scraper.buffer), nil
+	return scraper.buffer.String(), nil
 }
